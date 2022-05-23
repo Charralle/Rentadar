2bd926bce735da6d59b2f48f53bda2a35eddf18c
Charlesalerxandre Tiago <charles-alexandre.tiago@hotmail.com>
2022-05-23 17:36:42 +0200

    daron method

--- app/controllers/darons_controller.rb
+++ app/controllers/darons_controller.rb
@@ -5,8 +5,41 @@ class DaronsController < ApplicationController
   end
 
   def new
+    @daron = Daron.new
   end
 
   def create
+    @daron = Daron.new(daron_params)
+    if @daron.save
+      redirect_to daron_path(@daron)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @daron = Daron.find(params[:id])
+  end
+
+  def update
+    @daron = Daron.find(params[:id])
+    @daron.update(daron_params)
+    if @daron.update(daron_params)
+      redirect_to daron_path(@daron)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @daron = Daron.find(params[:id])
+    @daron.destroy!
+    redirect_to lists_path, status: :see_other
+  end
+
+  private
+
+  def list_params
+    params.require(:daron).permit(:name, :profile_picture, :bio, :price)
   end
 end
